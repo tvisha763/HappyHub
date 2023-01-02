503cacc48f953c04164d5aa0a50f31c34fa0240a
tvisha763 <tc9083@pleasantonusd.net>
2023-01-02 14:41:39 -0800

    17th commit

--- app2/settings.py
+++ app2/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'c=je$8)jmem%+^=yrc0#z@vl3$c#1p1(vl3=m@8)^op@mpjj-('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -123,8 +123,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiless')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
