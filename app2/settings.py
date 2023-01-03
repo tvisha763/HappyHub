0b9458cafe235330f6d85e5f4ea8b2d91e9021c8
tvisha763 <tc9083@pleasantonusd.net>
2023-01-02 17:23:51 -0800

    26th commit

--- app2/settings.py
+++ app2/settings.py
@@ -146,6 +146,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent # or manually set the path of 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 BASE_COUNTRY = "USA"
-
+CSRF_TRUSTED_ORIGINS = ["https://happyhub.herokuapp.com"]
 import django_heroku
 django_heroku.settings(locals())
