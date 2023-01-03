1b686c628c724376934429589e1c7f935c70e4f2
tvisha763 <tc9083@pleasantonusd.net>
2023-01-02 17:00:03 -0800

    23rd commit

--- app2/settings.py
+++ app2/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "APP/static")
 ]
 
 # Default primary key field type
